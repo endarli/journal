de27a56d731a753960bcc03135b840852c7a789b
Endar <endar.li215@gmail.com>
2026-02-21 03:48:24 -0500

    more prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,25 @@ import type { Metadata } from 'next';
 import '../index.css';
 
 export const metadata: Metadata = {
-  title: 'React App',
-  description: 'Web site created with Next.js.',
+    title: 'React App',
+    description: 'Web site created with Next.js.',
 };
 
 export const viewport: Viewport = {
-  themeColor: '#000000',
+    themeColor: '#000000',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
     children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
+    return (
+        <html lang="en">
             <head></head>
-      <body>
-        <div id="root">{children}</div>
-        {/*
+            <body>
+                <div id="root">{children}</div>
+                {/*
           This HTML file is a template.
           If you open it directly in the browser, you will see an empty page.
 
@@ -30,7 +30,7 @@ export default function RootLayout({
           To begin the development, run `npm start` or `yarn start`.
           To create a production bundle, use `npm run build` or `yarn build`.
         */}
-      </body>
-    </html>
-  );
+            </body>
+        </html>
+    );
 }
