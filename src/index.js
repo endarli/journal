5ac0ea16cce9943c93aa8598261e25a37f21cde2
Endar <endar.li215@gmail.com>
2026-02-22 02:20:10 -0500

    trying to add env keys

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'), {
+  onCaughtError: (error, errorInfo) => {
+    if (error.message !== 'Known error') {
+      reportCaughtError({
+        error,
+        componentStack: errorInfo.componentStack,
+      });
+    }
+  },
+});
+
 root.render(
   <React.StrictMode>
     <App />
