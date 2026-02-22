0a60a781337793c315d5f58d700281cde3366480
Endar <endar.li215@gmail.com>
2026-02-22 01:43:42 -0500

    decrease indent size

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,7 +10,7 @@ export default defineConfig([
     extends: ['ts/recommended', 'eslint:recommended'],
     rules: {
       'consistent-return': 2,
-      indent: [1, 4],
+      indent: [1, 2],
       'no-else-return': 1,
       semi: [1, 'always'],
       'space-unary-ops': 2,
