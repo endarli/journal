baad98820ca1d6b4a039b611c6216019d9e9e7a2
Endar <endar.li215@gmail.com>
2026-02-22 17:56:20 -0500

    brought back single quotes

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,45 +1,45 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     root: true,
     extends: [
-      "ts/recommended",
-      "eslint:recommended",
-      "plugin:eslint-plugin-react/recommended",
-      "prettier",
+      'ts/recommended',
+      'eslint:recommended',
+      'plugin:eslint-plugin-react/recommended',
+      'prettier',
     ],
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-      project: "./tsconfig.json",
-      sourceType: "module",
+      project: './tsconfig.json',
+      sourceType: 'module',
     },
-    plugins: ["@typescript-eslint", "@eslint/ts", "react"],
+    plugins: ['@typescript-eslint', '@eslint/ts', 'react'],
     rules: {
-      semi: [1, "always"],
-      "prefer-const": "error",
-      "@typescript-eslint/ban-ts-comment": "error",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      semi: [1, 'always'],
+      'prefer-const': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          args: "all",
-          argsIgnorePattern: "^_",
-          caughtErrors: "all",
-          caughtErrorsIgnorePattern: "^_",
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
           ignoreRestSiblings: true,
         },
       ],
-      quotes: ["error", "double"],
+      quotes: ['error', 'single'],
       // indent: [1, 2],
-      "space-unary-ops": ["warn", { words: true, nonwords: false }],
-      "sort-imports": [
-        "error",
+      'space-unary-ops': ['warn', { words: true, nonwords: false }],
+      'sort-imports': [
+        'error',
         {
           ignoreCase: false,
           ignoreDeclarationSort: false,
           ignoreMemberSort: false,
-          memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
         },
       ],
     },
