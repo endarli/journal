1a16d3326c1248d2cb606c50fb6558ec493e99e8
Endar <endar.li215@gmail.com>
2026-02-27 03:25:15 -0500

    better typing

--- src/components/CatContainer/CatContainer.tsx
+++ src/components/CatContainer/CatContainer.tsx
@@ -2,31 +2,30 @@
 import styles from './CatContainer.module.css';
 import { CatMetadata } from '../../services/cat.types';
 
-export function CatContainer(params: { catInfo: CatMetadata }) {
-  const catObject = params.catInfo;
+export function CatContainer({url, breeds}: CatMetadata) {
   return (
     <div className={styles.catContainer}>
       <h3>Cute Cat</h3>
       <img
         className={styles.catImage}
-        src={catObject.url}
+        src={url}
         as="img"
-        alt={catObject.breeds[0].name}
+        alt={breeds[0].name}
       />
       <div className={styles.catDetails}>
-        <h3>{catObject.breeds[0].name}</h3>
-        <p>Temperament: {catObject.breeds[0].temperament}</p>
-        <p>Description: {catObject.breeds[0].description}</p>
-        <p>Origin: {catObject.breeds[0].origin}</p>
+        <h3>{breeds[0].name}</h3>
+        <p>Temperament: {breeds[0].temperament}</p>
+        <p>Description: {breeds[0].description}</p>
+        <p>Origin: {breeds[0].origin}</p>
         <p>
-          Hypoallergenic: {catObject.breeds[0].hypoallergenic ? 'Yes' : 'No'}
+          Hypoallergenic: {breeds[0].hypoallergenic ? 'Yes' : 'No'}
         </p>
         <a
-          href={catObject.breeds[0].wikipedia_url}
+          href={breeds[0].wikipedia_url}
           target="_blank"
           rel="noreferrer"
         >
-          Learn more about the {catObject.breeds[0].name}
+          Learn more about the {breeds[0].name}
         </a>
       </div>
     </div>
